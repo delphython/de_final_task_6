222b4e5103538545169a6324eab5653773519966
Vitaly Klyukin <delphython@gmail.com>
2024-12-07 18:32:16 +0300

    redirect stdout to file & terminal

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -6,22 +6,30 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-echo "Отчет о логе веб сервера"
-echo "========================"
+report_file="report.txt"
 
-# Считаем общее количество запросов.
-awk 'END {print "Общее количество запросов:\t" NR}' $1
+{
+  echo "Отчет о логе веб сервера"
+  echo "========================"
 
-# Считаем количество уникальных IP-адресов.
-awk '(NR > 1) {vals[$1]} END {print "Количество уникальных IP-адресов:\t" length(vals)}' $1
+  # Считаем общее количество запросов.
+  result_1="$(awk 'END {print "Общее количество запросов: " NR}' $1)"
+  echo "$result_1"
 
-echo $'\n'
+  # Считаем количество уникальных IP-адресов.
+  result_2="$(awk '(NR > 1) {vals[$1]} END {print "Количество уникальных IP-адресов: " length(vals)}' $1)"
+  echo "$result_2"
 
-# Подсчитать количество запросов по методам
-echo "Количество запросов по методам:"
-awk -F '"' 'NF>2 {print $2}' $1 | awk '{ a[$1]++ }END{ for(i in a) printf "   %s\t%s\n",a[i], i }'
+  echo $'\n'
 
-echo $'\n'
+  # Подсчитать количество запросов по методам
+  echo "Количество запросов по методам:"
+  result_3="$(awk -F '"' 'NF>2 {print $2}' $1 | awk '{ a[$1]++ }END{ for(i in a) printf "   %s\t%s\n",a[i], i }')"
+  echo "$result_3"
 
-# Найти самый популярный URL
-awk -F '"' 'NF>2 {print $2}' access.log | awk '{ a[$2]++ }END{ for(i in a) printf "%s %s\n",a[i], i }' | awk 'BEGIN {max = 0} {if ($1>max) max=$1} END {printf "Самый популярный URL:\t%s %s\n", max, $2}'
+  echo $'\n'
+
+  # Найти самый популярный URL
+  result_4="$(awk -F '"' 'NF>2 {print $2}' $1 | awk '{ a[$2]++ }END{ for(i in a) printf "%s %s\n",a[i], i }' | awk 'BEGIN {max = 0} {if ($1>max) max=$1} END {printf "Самый популярный URL:\t%s %s\n", max, $2}')"
+  echo "$result_4"
+} 2>&1 | tee -- "$report_file"
