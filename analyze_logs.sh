55a0badd744059af8b29aab0f97c5e7ac1c95a44
Vitaly Klyukin <delphython@gmail.com>
2024-12-07 13:56:58 +0300

    add unique ip count

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-while IFS= read -r line
-do
-  echo "$line"
-done < $1
+# while IFS= read -r line
+# do
+#   echo "$line"
+# done < $1
+
+# Проверяем, передан ли аргумент
+if [ -z "$1" ]; then
+    echo "Пожалуйста, укажите файл для анализа."
+    exit 1
+fi
+
+# Считаем количество уникальных IP-адресов.
+ awk '(NR > 1) {vals[$1]} END {print "количество уникальных IP-адресов:\t" length(vals)}' access.log
